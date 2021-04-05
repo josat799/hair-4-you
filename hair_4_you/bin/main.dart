aa90f746ec5e2dc84726669ca1201c7142ce4555
Josef Atoui - MAC <josef.atoui@live.se>
2021-04-05 14:52:56 +0200

    Added aqueduct template

---
+++ hair_4_you/bin/main.dart
@@ -0,0 +1,13 @@
+import '../lib/hair_4_you.dart';
+
+Future main() async {
+  final app = Application<Hair4YouChannel>()
+    ..options.configurationFilePath = "config.yaml"
+    ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
