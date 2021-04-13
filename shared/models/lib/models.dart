5f893144c168cdc66f2e5ae0ec9517b68da71e29
Josef Atoui - MAC <josef.atoui@live.se>
2021-04-13 20:50:54 +0200

    Added support for Aqueduct PostgreSQL

--- shared/models/lib/models.dart
+++ shared/models/lib/models.dart
@@ -2,5 +2,6 @@ library models;
 
 export 'package:models/user_models/user.dart';
 export 'package:models/booking_models/booking.dart';
+export 'package:aqueduct/aqueduct.dart';
 
 export 'dart:convert';
