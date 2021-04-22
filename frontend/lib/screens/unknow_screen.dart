778ec8bf2b6fedfd4b76a0836d0af94f2f3a6315
Josef Atoui - MAC <josef.atoui@live.se>
2021-04-22 15:03:06 +0200

    Added 404 page

---
+++ frontend/lib/screens/unknow_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UnknowScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('404 Page not found'),
+        ),
+      ),
+    );
+  }
+}
