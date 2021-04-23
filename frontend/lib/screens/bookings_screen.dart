c801e6e5a3890fec9e4bb8ae2ed0f5248a9d18a2
Josef Atoui - MAC <josef.atoui@live.se>
2021-04-23 14:59:32 +0200

    Added booking screen and added route

---
+++ frontend/lib/screens/bookings_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingsScreen extends StatelessWidget {
+  static const ROUTENAME = "/bookings";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
