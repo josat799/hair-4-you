a394e0724b316980db5af855e5afb61e5d702858
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-03 15:38:58 +0200

    Fixed viewable list of bookings

--- frontend/lib/screens/bookings_screen.dart
+++ frontend/lib/screens/bookings_screen.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/services/booking_service.dart';
 
-class BookingsScreen extends StatelessWidget {
+class BookingsScreen extends StatefulWidget {
   static const ROUTENAME = "/bookings";
 
-  Future<List<Map<String, dynamic>>> _getBookings(BuildContext context) async {
-    return await BookingService(context).fetchBookings();
+  @override
+  _BookingsScreenState createState() => _BookingsScreenState();
+}
+
+class _BookingsScreenState extends State<BookingsScreen> {
+  Future<List<dynamic>> _futureList;
+
+  @override
+  initState() {
+    super.initState();
+    _futureList = BookingService(context).fetchBookings();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: _getBookings(context),
-      builder: (context, snapshot) => Center(
-        child: snapshot.data == null
-            ? CircularProgressIndicator()
-            : ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (_, index) => Card(
-                  child: Text('${snapshot.data[index]['name']}'),
+    return FutureBuilder<List<dynamic>>(
+        future: _futureList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (_, index) => Center(
+                child: Card(
+                  child: Text('${snapshot.data[index]['title']}'),
                 ),
               ),
-      ),
-    );
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
