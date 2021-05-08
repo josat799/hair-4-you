948577d42f7779ce30a82af4f5c593e1f713ea95
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-08 12:09:27 +0200

    Fixes json decoder method

--- frontend/lib/models/user.dart
+++ frontend/lib/models/user.dart
@@ -25,10 +25,15 @@ class User {
   User.fromJson(Map<String, dynamic> json)
       : name = json['name'],
         email = json['email'],
-        birthDate =
-            json['birthDate'] == null ? null : jsonDecode(json['birthDate']),
-        createdAt = jsonDecode(json['createdAt']),
-        lastLoggedIn = jsonDecode(json['lastLoggedIn']),
+        birthDate = json['birthDate'] == null
+            ? null
+            : DateTime.parse(json['birthDate']),
+        createdAt = json['createdAt'] == null
+            ? null
+            : DateTime.parse(json['createdAt']),
+        lastLoggedIn = json['lastLoggedIn'] == null
+            ? null
+            : DateTime.parse(json['lastLoggedIn']),
         phoneNumber = json['phoneNumber'],
         id = json['id'];
 
