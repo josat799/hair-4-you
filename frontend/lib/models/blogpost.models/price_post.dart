48565ee60924d81468a0fc8905b0d381a2d4a93a
josat799 <josef.atoui@live.se>
2021-05-21 13:46:20 +0200

    Adds price and blog class

---
+++ frontend/lib/models/blogpost.models/price_post.dart
@@ -0,0 +1,10 @@
+import 'package:frontend/models/blogpost.models/post.dart';
+import 'package:frontend/models/user.dart';
+
+class PricePost extends Post {
+  double price;
+
+  PricePost(this.price, {int? id, String? title, String? description, User? author,
+      DateTime? createdAt})
+      : super(id, title, description, author, createdAt);
+}
