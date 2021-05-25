07c489503cdf0934cbe2d8fc906aaa9924f5630f
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-25 15:20:17 +0200

    Adds support for logging in with google

--- frontend/lib/services/OAuth.dart
+++ frontend/lib/services/OAuth.dart
@@ -5,6 +5,7 @@ import 'package:frontend/models/user.dart';
 import 'package:frontend/providers/user_auth.dart';
 import 'package:frontend/services/user_service.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -50,6 +51,26 @@ class OAuth {
       "statusCode": response.statusCode
     };
   }
+
+  Future<bool> verifyGoogle(String token) async {
+    final String clientID = "${context.read<UserAuth>().clientID}";
+    String body = "google_token=$token";
+    final String clientCredentials =
+        const Base64Encoder().convert("$clientID:".codeUnits);
+
+    final Response response = await http.post(
+      Uri.http('localhost:8888', "/google"),
+      body: body,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic $clientCredentials",
+      },
+    );
+
+    return response.statusCode == 200;
+  }
+
+  Future<void> loginWithGoogle() async {}
 
   Future<void> login(String username, String password) async {
     context.read<UserAuth>().userState = UserState.loggingIn;
