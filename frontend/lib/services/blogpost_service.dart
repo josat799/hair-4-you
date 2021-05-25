506b7f875622b46beaf16c46ae8131f1f928aac6
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-26 00:44:05 +0200

    Adds support for retriving blogposts

--- frontend/lib/services/blogpost_service.dart
+++ frontend/lib/services/blogpost_service.dart
@@ -1,34 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/models/blogpost.models/blog_post.dart';
+import 'package:frontend/providers/user_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class BlogPostService {
   final BuildContext context;
-  final List<BlogPost> _blogPost = [
-    BlogPost(
-      1,
-      "Hi",
-      "",
-      null,
-      DateTime.now().subtract(
-        Duration(days: 1),
-      ),
-    ),
-    BlogPost(
-      2,
-      "Hello World!",
-      "Hello and welcome to the world!",
-      null,
-      DateTime.now(),
-    ),
-  ];
+
   BlogPostService(this.context);
 
-  BlogPost getBlogPostByID(int id) {
-    return _blogPost.firstWhere((element) => element.id == id);
-  }
+  Future<List<BlogPost>> fetchBlogPosts({int? blogPostID}) async {
+    String path;
+    Map<String, String> headers;
 
-  List<BlogPost> fetchBlogPost() {
-    //TODO: make request to backend
-    return _blogPost;
+    if (blogPostID == null) {
+      path = "/blogposts";
+      final clientID = context.read<UserAuth>().clientID;
+      final String clientCredentials =
+          const Base64Encoder().convert("$clientID:".codeUnits);
+      headers = {'Authorization': 'Basic $clientCredentials'};
+    } else {
+      path = "/restricted/blogposts/$blogPostID";
+      final String token = context.read<UserAuth>().token;
+      headers = {'Authorization': 'Bearer $token'};
+    }
+    var response = await http.get(
+        Uri.http(
+          "localhost:8888",
+          path,
+        ),
+        headers: headers);
+
+    if (response.statusCode == 200) {
+      dynamic decodedBody = json.decode(response.body);
+      if (blogPostID == null) {
+        Iterable<dynamic> l = decodedBody;
+        List<BlogPost> posts = List<BlogPost>.from(
+            l.map((blogPost) => BlogPost.fromJson(blogPost)));
+        return posts;
+      } else {
+        return [BlogPost.fromJson(decodedBody)];
+      }
+    }
+    return [];
   }
 }
