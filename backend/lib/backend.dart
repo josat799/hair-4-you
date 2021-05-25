ba0987b99f900d2e4501939aa9ee856f2c5280ed
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-25 21:53:50 +0200

    Removes old models

--- backend/lib/backend.dart
+++ backend/lib/backend.dart
@@ -9,11 +9,16 @@ export 'dart:io';
 export 'package:aqueduct/aqueduct.dart';
 export 'package:aqueduct/managed_auth.dart';
 
-
-export 'package:models/models.dart';
+export 'package:backend/models/post_models/blogpost.dart';
+export 'package:backend/models/booking_models/booking.dart';
+export 'package:backend/models/user_models/user.dart';
+export 'package:backend/models/relations/booking_customer.dart';
+export 'package:backend/models/relations/booking_hairdresser.dart';
 
 export 'package:backend/controllers/booking_controller.dart';
 export 'package:backend/controllers/user_controller.dart';
 export 'package:backend/controllers/register_controller.dart';
 export 'package:backend/controllers/google_controller.dart';
+export 'package:backend/controllers/blogpost_controller.dart';
+
 export 'channel.dart';
