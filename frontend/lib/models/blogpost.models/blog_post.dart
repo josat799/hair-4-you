a5a90bc89760d6f8b587954a9df9f83dafb14e9e
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-27 00:20:43 +0200

    Adds blogpost model for frontend

--- frontend/lib/models/blogpost.models/blog_post.dart
+++ frontend/lib/models/blogpost.models/blog_post.dart
@@ -2,7 +2,30 @@ import 'package:frontend/models/blogpost.models/post.dart';
 import 'package:frontend/models/user.dart';
 
 class BlogPost extends Post {
-  BlogPost(int? id, String? title, String? description, User? author,
-      DateTime? createdAt)
-      : super(id, title, description, author, createdAt);
+  BlogPost(
+    this.visiable,
+    int? id,
+    String? title,
+    String? description,
+    User? author,
+    DateTime? createdAt,
+  ) : super(
+          id,
+          title,
+          description,
+          author,
+          createdAt,
+        );
+
+  BlogPost.fromJson(Map<String, dynamic> json)
+      : this.visiable = json['visiable'],
+        super(
+          json['id'],
+          json['title'],
+          json['description'],
+          User.fromJson(json['author']),
+          DateTime.parse(json['createdAt']),
+        );
+
+  bool visiable;
 }
