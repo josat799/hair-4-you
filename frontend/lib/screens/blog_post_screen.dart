7835533f3c2a8fc4c35ce898dfa91fa68f3f3043
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-27 23:29:24 +0200

    Adds blogpost screen

---
+++ frontend/lib/screens/blog_post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BlogPostScreen extends StatelessWidget {
+  static const ROUTENAME = '/blogposts';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
