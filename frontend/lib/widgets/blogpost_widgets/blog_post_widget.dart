2f39df640c53aedca81ff280bfb3d9b87d6bba11
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-27 23:12:09 +0200

    Updates modifiable blogposts

--- frontend/lib/widgets/blogpost_widgets/blog_post_widget.dart
+++ frontend/lib/widgets/blogpost_widgets/blog_post_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/blogpost.models/blog_post.dart';
+import 'package:frontend/models/user.dart';
+import 'package:frontend/providers/user_auth.dart';
+import 'package:frontend/widgets/forms_widgets/update_blogpost.dart';
+import 'package:provider/provider.dart';
 
 class SingleBlogPost extends StatefulWidget {
   final BlogPost blogPost;
@@ -15,24 +19,99 @@ class SingleBlogPost extends StatefulWidget {
 }
 
 class _SingleBlogPostState extends State<SingleBlogPost> {
-  bool expanded = false;
+
+  void _openDeleteDialog() {
+    () {
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text('Delete?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Delete',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    };
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      title: Center(child: Text("${widget.blogPost.title}")),
-      subtitle: Column(
-        children: [
-          Text("${widget.blogPost.description}"),
-          Text("${widget.blogPost.author!.name}"),
-        ],
+    bool canEdit = false;
+    User? loggedInUser = context.watch<UserAuth>().user;
+    if (loggedInUser != null &&
+        (loggedInUser.identical(widget.blogPost.author!))) {
+      canEdit = true;
+    }
+    return canEdit
+        ? ExpansionTile(
+            key: widget.key,
+     
+            title: customTitle(),
+            subtitle: customSubtitle(),
+            trailing: canEdit
+                ? Container(
+                    width: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.edit),
+                        InkWell(
+                          onTap: _openDeleteDialog,
+                          child: Icon(Icons.delete, color: Colors.red),
+                        )
+                      ],
+                    ),
+                  )
+                : null,
+            maintainState: true,
+            children: [
+              UpdateBlogPost(widget.blogPost),
+            ],
+          )
+        : ListTile(
+            title: customTitle(),
+            subtitle: customSubtitle(),
+          );
+  }
+
+  Widget customLeading() {
+    return Text('');
+  }
+
+  Widget customTitle() {
+    return Center(
+      child: Text.rich(
+        TextSpan(text: "${widget.blogPost.title}"),
       ),
-      trailing: Icon(Icons.edit),
-      maintainState: true,
+    );
+  }
+
+  Widget customSubtitle() {
+    return Column(
       children: [
-        TextFormField(),
-        TextFormField(),
-        TextFormField(),
+        Text.rich(
+          TextSpan(text: "${widget.blogPost.description}"),
+        ),
+        Text.rich(
+          TextSpan(text: "${widget.blogPost.author!.name}"),
+        ),
       ],
     );
   }
