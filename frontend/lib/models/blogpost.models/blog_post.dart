e161065945a34c02a2abd7c09afbccc7a5b2e804
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-27 23:08:11 +0200

    Adds toJson for blogpost model

--- frontend/lib/models/blogpost.models/blog_post.dart
+++ frontend/lib/models/blogpost.models/blog_post.dart
@@ -2,14 +2,14 @@ import 'package:frontend/models/blogpost.models/post.dart';
 import 'package:frontend/models/user.dart';
 
 class BlogPost extends Post {
-  BlogPost(
-    this.visiable,
+  BlogPost({
+    required this.visiable,
     int? id,
     String? title,
     String? description,
     User? author,
     DateTime? createdAt,
-  ) : super(
+  }) : super(
           id,
           title,
           description,
@@ -26,6 +26,16 @@ class BlogPost extends Post {
           User.fromJson(json['author']),
           DateTime.parse(json['createdAt']),
         );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'visiable': visiable,
+      'id': id,
+      'title': title,
+      'description': description,
+      'createdAt': createdAt.toString(),
+    };
+  }
 
   bool visiable;
 }
