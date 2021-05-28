7d2362ec49a4b42865f0eba22ee8b042f93e143e
Josef Atoui - MAC <josef.atoui@live.se>
2021-05-28 22:49:04 +0200

    Adds pricepost model and schema to backend

---
+++ backend/migrations/00000006_unnamed.migration.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'package:aqueduct/aqueduct.dart';   
+
+class Migration6 extends Migration { 
+  @override
+  Future upgrade() async {
+   		database.createTable(SchemaTable("Pricepost", [SchemaColumn("price", ManagedPropertyType.doublePrecision, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("id", ManagedPropertyType.bigInteger, isPrimaryKey: true, autoincrement: true, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("title", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("description", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("createdAt", ManagedPropertyType.datetime, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: true)]));
+		database.addColumn("Pricepost", SchemaColumn.relationship("author", ManagedPropertyType.bigInteger, relatedTableName: "Users", relatedColumnName: "id", rule: DeleteRule.nullify, isNullable: true, isUnique: false));
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
