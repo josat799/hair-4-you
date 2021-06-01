1999b3d714818f27cdf3940f7c3242a2649f2528
Josef Atoui - MAC <josef.atoui@live.se>
2021-06-01 16:38:42 +0200

    Updates index screen

--- frontend/lib/screens/index_screen.dart
+++ frontend/lib/screens/index_screen.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:frontend/models/blogpost.models/price_post.dart';
 import 'package:frontend/providers/user_auth.dart';
 import 'package:frontend/services/user_service.dart';
-import 'package:frontend/widgets/blogpost_widgets/blog_post_multiple.dart';
+import 'package:frontend/widgets/post_widgets/multiple_post.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/widgets/login_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -46,7 +47,7 @@ class _IndexScreenState extends State<IndexScreen> {
         ],
       ),
       body: Center(
-        child: BlogPostMultiple(onlyVisiable: true,),
+        child: MultiplePosts<PricePost>(),
       ),
     );
   }
