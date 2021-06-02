fc82adda9b0e0e2f9fb2d3897c2b73385d0bfd0f
Josef Atoui - MAC <josef.atoui@live.se>
2021-06-02 16:25:08 +0200

    Updates indexscreen

--- frontend/lib/screens/index_screen.dart
+++ frontend/lib/screens/index_screen.dart
@@ -32,7 +32,9 @@ class _IndexScreenState extends State<IndexScreen> {
         ],
       ),
       body: Center(
-        child: MultiplePosts<PricePost>(),
+        child: MultiplePosts<PricePost>(
+          updateFrequency: 10,
+        ),
       ),
     );
   }
