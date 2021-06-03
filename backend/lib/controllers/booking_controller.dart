2a71a831ab346e5e94fc866c19eb43b956e6eb8c
Josef Atoui - MAC <josef.atoui@live.se>
2021-06-03 22:40:48 +0200

    Adds information about customer and hairdresser to booking

--- backend/lib/controllers/booking_controller.dart
+++ backend/lib/controllers/booking_controller.dart
@@ -8,7 +8,21 @@ class BookingController extends ResourceController {
   @Operation.get('id')
   Future<Response> getBookingByID(@Bind.path('id') int id) async {
     final query = Query<ManagedBooking>(context)
-      ..where((booking) => booking.id).equalTo(id);
+      ..where((booking) => booking.id).equalTo(id)
+      ..join(set: (booking) => booking.bookingCustomer)
+          .join<ManagedUser>(
+              object: (bookingCustomer) => bookingCustomer.customer)
+          .returningProperties((customer) => [
+                customer.name,
+                customer.email,
+              ])
+      ..join(set: (booking) => booking.bookingHairdresser)
+          .join<ManagedUser>(
+              object: (bookingHairDresser) => bookingHairDresser.hairDresser)
+          .returningProperties((hairdresser) => [
+                hairdresser.name,
+                hairdresser.email,
+              ]);
 
     final booking = await query.fetchOne();
 
@@ -26,12 +40,14 @@ class BookingController extends ResourceController {
               object: (bookingCustomer) => bookingCustomer.customer)
           .returningProperties((customer) => [
                 customer.name,
+                customer.email,
               ])
       ..join(set: (booking) => booking.bookingHairdresser)
           .join<ManagedUser>(
               object: (bookingHairDresser) => bookingHairDresser.hairDresser)
           .returningProperties((hairdresser) => [
                 hairdresser.name,
+                hairdresser.email,
               ]);
     return Response.ok(await query.fetch());
   }
@@ -77,7 +93,7 @@ class BookingController extends ResourceController {
       final relationQuery = Query<ManagedBookingCustomer>(context)
         ..values = bookingCustomerRelation;
 
-      bookingQuery.values.bookedAmountOfCustomers++;
+      _booking.bookedAmountOfCustomers++;
 
       await relationQuery.insert();
     } else if (request.path.segments.contains('hairdresser')) {
